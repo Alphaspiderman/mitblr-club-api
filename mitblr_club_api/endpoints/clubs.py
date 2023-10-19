d370ce7fde7871bf25a509ba9f83771a51c243e8
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2023-10-20 00:11:24 +0530

    Basic operations of clubs endpoint added

--- mitblr_club_api/endpoints/clubs.py
+++ mitblr_club_api/endpoints/clubs.py
@@ -2,27 +2,83 @@ from typing import Optional
 
 from sanic.request import Request
 from sanic.views import HTTPMethodView
+from sanic.response import json
+from sanic_ext import validate
+
 
 from mitblr_club_api.app import appserver
 
+from mitblr_club_api.models.clubs import Club_Create
+from mitblr_club_api.decorators.authorized import authorized_incls
+
 
 class Clubs(HTTPMethodView):
+
+    @authorized_incls
     async def get(self, request: Request, slug: Optional[str]):
         """Get Club Info"""
         collection = request.app.ctx.db["clubs"]
         if slug == "":
             # Get all clubs
             # flake8: noqa
-            doc = await collection.find({{}, {"coursename": 1}})
-            pass
+            docs = await collection.find({}).to_list(length=100)
+            
+            if docs is None:
+                return json({"Code": "404", "Message": "No Events Found"}, status=404)
+            else:
+                club_list = []
+                for doc in docs:
+                    club_list.append(
+                        {
+                            "club": doc["name"],
+                            "unit": doc["unit_type"],
+                            "institution": doc["institution"]
+                        }
+                    )
+                
+                return json(club_list)
+
         else:
             # Get specific club
-            pass
+            doc = await collection.find_one({"slug": slug})
+            if not doc:
+                return json({"Code": "404", "Message": "No Events Found"}, status=404)
+            else:
+                return json(
+                            {
+                                "club": doc["name"],
+                                "unit": doc["unit_type"],
+                                "institution": doc["institution"]
+                            }
+                        )
 
     # TODO - Data Validation
     # TODO - Authentication
-    async def post(self, request: Request, slug: Optional[str]):
+
+    @authorized_incls
+    @validate(json=Club_Create)
+    async def post(self, request: Request, body: Club_Create, slug: Optional[str]):
         """Create Clubs"""
+        collection = request.app.ctx.db["clubs"]
+        doc = await collection.find_one({"slug": body.slug})
+        
+        if doc is None:
+            club = dict()
+            club["name"] = body.name
+            club["slug"] = body.slug
+            club["unit_type"] = body.unit_type.value
+            club["institution"] = body.institution
+            club["faculty_advisors"] = body.faculty_advisors
+            club["core_committee"] = {}
+            club["events"] = {}
+            club["operations"] = []
+            result = await collection.insert_one(club)
+            d = {"Insert": "True", "ObjectId": str(result.inserted_id)}
+            return json(d)
+        
+        else:
+            d = {"Error Code": "409", "Message": "Conflict - Object already exists"}
+            return json(d, status=409)
 
     # TODO - Data Validation
     # TODO - Authentication
--- mitblr_club_api/models/clubs.py
+++ mitblr_club_api/models/clubs.py
@@ -32,3 +32,14 @@ class Club(BaseModel):
 
     class Config:
         arbitrary_types_allowed = True
+
+class Club_Create(BaseModel):
+    faculty_advisors: list[dict[str, str]]
+    institution: str
+    name: str
+    slug: str
+    unit_type: Unit
+
+    class Config:
+        arbitrary_types_allowed = True
+        
