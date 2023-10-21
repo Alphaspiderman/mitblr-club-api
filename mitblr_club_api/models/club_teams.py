601a19acfe4144b4068d00eeb6aa413761ab375a
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2023-10-21 16:45:44 +0530

    Added post(partial) for club_teams Will work on completion after resolution of issue #18

--- mitblr_club_api/models/club_teams.py
+++ mitblr_club_api/models/club_teams.py
@@ -16,3 +16,19 @@ class ClubTeam(BaseModel):
     permissions: dict[str, bool]
     position: dict[str, str]
     student_id: str
+    class Config:
+        arbitrary_types_allowed = True
+
+class ClubTeam_Create(BaseModel):
+    """
+    ClubTeam document creation model.
+    """
+
+    api_access: bool
+    club: str
+    permissions: dict[str, bool]
+    position: dict[str, str]
+    class Config:
+        arbitrary_types_allowed = True
+
+    
