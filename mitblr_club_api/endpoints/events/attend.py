d726a4ec72b572aac8938877b7501f42053f74b3
Hari Keshav Rajesh <harikeshav.rajesh@gmail.com>
2023-11-09 00:02:08 +0530

    Modifiy EventsAttend post to update events collection

--- mitblr_club_api/endpoints/events/attend.py
+++ mitblr_club_api/endpoints/events/attend.py
@@ -6,6 +6,8 @@ from sanic.views import HTTPMethodView
 
 from mitblr_club_api.decorators.authorized import authorized_incls
 
+from bson import ObjectId
+
 
 class EventsAttend(HTTPMethodView):
     """Endpoints regarding event attendance."""
@@ -91,6 +93,7 @@ class EventsAttend(HTTPMethodView):
 
         event = await events.find_one({"slug": slug})
 
+        # Checking if event exists
         if not event:
             return json(
                 {"status": 404, "error": "Not Found", "message": "No events found."},
@@ -100,6 +103,7 @@ class EventsAttend(HTTPMethodView):
         # TODO - Timed Cache
         student = await students.find_one({"application_number": str(uuid)})
 
+        # Checking if student exists
         if not student:
             return json(
                 {"status": 404, "error": "Not Found", "message": "No student found."},
@@ -108,6 +112,19 @@ class EventsAttend(HTTPMethodView):
 
         event_id = event["_id"]
 
+        # Checking if student attendance is already marked
+        for attendee in event["participants"]["attended"]:
+            if attendee == student["_id"]:
+                return json(
+                    {
+                        "status": 409,
+                        "error": "Conflict",
+                        "message": "Student attendance is already marked.",
+                    },
+                    status=409,
+                )
+
+        # Updating attendance in events field in student document in students collection
         for event in student["events"]:
             if event["event_id"] == event_id:
                 # Match the specific event_id within the events array.
@@ -117,14 +134,19 @@ class EventsAttend(HTTPMethodView):
                 }
 
                 # Update the attendance field of the matched element.
-                update = {"$set": {"events.$.attendance": "true"}}
+                update = {"$set": {"events.$.attended": True}}
 
                 await students.update_one(query, update)
+
+                # Update on event object in the events collection.
+                await events.update_one(
+                    {"_id": event_id},
+                    {"$push": {"participants.attended": ObjectId(student["_id"])}},
+                )
                 return json(
                     {"status": 200, "message": "Student attendance has been updated."}
                 )
 
-            # TODO: Update on event object in the events collection.
             else:
                 # TODO: Mark as on-spot registration.
                 pass
